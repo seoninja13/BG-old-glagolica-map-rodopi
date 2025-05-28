bed3a1af6c17d10b38fdb6ce1acc611e42b596ff
seoninja13 <dachevivo@gmail.com>
2025-05-27 17:14:14 -0700

    Add _app.js to import global styles (Tailwind)

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+// Import global styles, including Tailwind CSS
+import '../styles/globals.css';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
